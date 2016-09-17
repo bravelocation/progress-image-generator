7d36e77a6d9ab67a81ffc8bf6245b49ff3b736dd
John Pollard <johnp@bravelocation.com>
2016-09-17 10:41:08 +0100

    Initial Commit

---
+++ ProgressImageGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProgressImageGenerator
+//
+//  Created by John Pollard on 17/09/2016.
+//  Copyright © 2016 Bravelocation Software Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
